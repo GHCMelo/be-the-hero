df9409a6c7d56a2a12fb0f8d0868765b3368099e
GHCMelo <gabcardosomelo@gmail.com>
2020-04-09 20:37:33 -0300

    Back-end da aplicação

---
+++ Back-end/src/routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const OngController = require('./controller/OngController');
+const IncidentController = require('./controller/IncidentController');
+const ProfileController = require('./controller/ProfileController');
+const SessionController = require('./controller/SessionController');
+
+const routes = express.Router();
+
+// Rota login
+routes.post('/sessions', SessionController.create);
+
+// Rotas ongs
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+// Rota ong específica
+routes.get('/profile', ProfileController.index);
+
+// Rotas incidentes ongs
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+
+module.exports = routes;
